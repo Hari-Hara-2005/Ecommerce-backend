af4093cb98016c123aea1ddc06a560a669ddfb0d
Hari-Hara-2005 <hhara1699@gmail.com>
2026-04-22 20:32:47 +0530

    Add delivery price management routes and controller

---
+++ routes/delivery.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/deliveryController');
+
+router.post('/delivery', controller.addDeliveryPrice);
+router.get('/delivery', controller.getAllDeliveryPrices);
+router.put('/delivery/:id', controller.updateDeliveryPrice);
+router.delete('/delivery/:id', controller.deleteDeliveryPrice);
+
+module.exports = router;
