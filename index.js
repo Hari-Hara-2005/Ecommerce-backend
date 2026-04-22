89f9a247e2ac861c809111eeb2ff8011ddb43cf2
Hari-Hara-2005 <hhara1699@gmail.com>
2026-04-22 21:36:44 +0530

    Refactor server initialization and route setup in index.js

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const express = require('express');
-const cors = require('cors');
-
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
 
-// Routes
-app.use('/api/athu', require('./routes/athu.routes'));
 app.use('/api', require('./routes/product.routes'));
 app.use('/api', require('./routes/order.routes'));
 app.use('/api', require('./routes/category.routes'));
 app.use('/api', require('./routes/cart.route'));
 app.use('/api', require('./routes/banner.routes'));
 app.use('/api', require('./routes/topbar.routes'));
-
-
-module.exports = serverless(app);
+app.listen(5000, () => {
+    console.log("The Serever Running in the Port : 5000");
+});
