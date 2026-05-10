3a41932ee007d4c944c998a3cc56b0763f33e66f
Hari-Hara-2005 <hhara1699@gmail.com>
2026-05-10 09:24:23 +0530

    Update Cloudinary configuration with new credentials

--- db/cloudinaryConfig.js
+++ db/cloudinaryConfig.js
@@ -1,7 +1,7 @@
 const cloudinary = require("cloudinary").v2;
 cloudinary.config({
-    cloud_name: 'dlahzfpmk',
-    api_key: '734567775688883',
-    api_secret: '5ox8XvHVTYKyGuzcmzy6giXQnzI'
+    cloud_name: 'di7cxkakc',
+    api_key: '441559275616635',
+    api_secret: 'JaYBULhNf78oQu1VnJZb9QoOW'
 });
 module.exports = cloudinary;
